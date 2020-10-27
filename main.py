fa271237db37201b6bb409c70ed2116794c02092
amandiof <amandio31123@gmail.com>
2020-10-27 11:48:47 -0300

    Add number of mamacos

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import tasks
+import asyncio
 
 client = discord.Client()
 
@@ -17,6 +19,23 @@ async def on_message(message):
         return
     if message.content.startswith('$dnama'):
         await message.channel.send(':craig:' + message.content[6:])
+
+async def nmb_of_mamacos():
+    while True:
+        chan_to_send = None
+        for text_chan in client.guilds[0].text_channels:
+            if text_chan.name == 'plebe':
+                chan_to_send = text_chan
+                break
+        msg = ""
+        for chan in client.guilds[0].voice_channels:
+            msg += chan.name + " tem " + str(len(chan.members)) + " mamacos.\n"
+        await chan_to_send.send(msg)
+        await asyncio.sleep(60)
+
+@client.event
+async def on_ready():
+    await nmb_of_mamacos()
 
 with open('token.key') as token:
     client.run(token.read())
